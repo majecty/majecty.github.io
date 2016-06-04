9a19a279eb122b3803be176493d606028f61c243
ParkJuhyeong <majecty@gmail.com>
2016-06-04 21:16:10 +0900

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,9 @@
+git branch -f master
+stack run site clean
+stack run site build
+cp -a _site/. .
+git checkout master
+git add .
+git commit -m 'Publish'
+git push origin develop
+git push origin master -f
